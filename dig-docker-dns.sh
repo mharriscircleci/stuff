39fe00213952022c4b32c6f122fb617f5b8dca9b
Meagan Harris <meagan@circleci.com>
2017-11-22 14:33:59 -0500

    add test to query a large RRset

--- dig-docker-dns.sh
+++ dig-docker-dns.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo testing regular lookup
 dig docker-registry.artifactory-infra.gopro-platform.com
---
+++ dig-mharrisnet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo check for message truncation
+dig TXT test1.meaganharris.net | grep Truncated
+
+
+
